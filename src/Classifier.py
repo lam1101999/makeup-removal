ee38f6145c4b630b6ab287bf368fe92e424daf6a
lam1101999 <nguyenvuduclam@gmail.com>
2023-11-24 11:06:00 +0700

    upload main page, classification

---
+++ src/Classifier.py
@@ -0,0 +1,67 @@
+import torchvision
+import torch
+import os
+from torchvision import transforms
+class Classifier():
+    def __init__(self):
+        self.weight_folder = os.path.join(os.getcwd(), "model_weight")
+        self.transform = transforms.Compose([
+            transforms.Resize((96, 96)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = None
+    def choose_model(self, model_name="resnet34"):
+        """
+        Selects and loads a pre-trained model based on the given `model_name`.
+
+        Parameters:
+            model_name (str): The name of the model to choose. Default is "resnet34".
+
+        Returns:
+            None
+        """
+        weight_path = os.path.join(self.weight_folder, f"{model_name}.pth")
+        model_state_dict = torch.load(
+            weight_path, map_location=torch.device(self.device)) 
+        if model_name == "resnet34":
+            self.model = torchvision.models.resnet34(num_classes=2)
+            self.model.load_state_dict(model_state_dict)
+            
+        else:
+            self.model = getattr(torchvision.models, model_name)(num_classes=2)
+            self.model.load_state_dict(model_state_dict)
+        
+    def predict(self, image):
+        """
+        Predicts the output for a given image.
+
+        Parameters:
+            image (Tensor): The input image to be predicted.
+
+        Returns:
+            output (Tensor): The predicted output of the model.
+        """
+        
+        if self.model is None:
+            raise Exception("Please choose a model first")
+        image = self.transform(image)
+        image = image[None, :]
+        self.model.eval()
+        with torch.no_grad():
+            output = self.model(image)
+            print("____________________")
+            print("Logit",output)
+            output = torch.nn.functional.softmax(output, dim=1)
+            print("softmax",output)
+            predict_class = torch.argmax(output, dim=1)
+            proba = output[:,predict_class].squeeze()
+            print(predict_class, proba)
+        return predict_class, proba
+    
+if __name__ == "__main__":
+    # Test this class
+    classifier = Classifier()
+    classifier.choose_model("efficientnet_v2_l")
+    print(classifier.model)
--- src/main.py
+++ src/main.py
@@ -1,39 +1,71 @@
 import streamlit as st
 from FaceDetector import FaceDetector
 from MakeupRemover import MakeupRemover
+from Classifier import Classifier
 from PIL import Image
 import numpy as np
 
 
 def main():
+
+    # Init models
     face_detector = FaceDetector()
     makeup_remover = MakeupRemover()
+    classifier = Classifier()
+    classifier.choose_model("resnet34")
+
+    # Decorate main page
     st.title("WebApp Makeup Remove")
     st.header("A Project in AI class")
-    image_file = st.file_uploader(
-        "Upload an image", type=["png", "jpg", "jpeg"])
-    if image_file is not None:
-        our_image = Image.open(image_file).convert("RGB")
-        st.markdown("Your input image:")
-        st.image(our_image, channels="RGB")
-        faces = face_detector.detect_face(our_image)
-        information = f"**Detect {len(faces)} faces:**"
-        st.markdown(information)
+    
+    # Sidebar
+    if "file_uploader_key" not in st.session_state:
+        st.session_state["file_uploader_key"] = 0
+    image_list = st.sidebar.file_uploader(
+        "Upload an image", type=["png", "jpg", "jpeg"], accept_multiple_files= True, key=st.session_state["file_uploader_key"])
+    if st.sidebar.button('Reset'):
+        st.session_state["file_uploader_key"] += 1
+        st.experimental_rerun()
 
-        for index, face in enumerate(faces):
-            # Remove makeup
-            remove_makeup = makeup_remover.remove_makeup(face["image"])
-            remove_makeup = Image.fromarray(
-                (remove_makeup*255).astype(np.uint8)).resize((face["width"], face["height"]))
+    # Sidebar to select model
+    @st.cache_data
+    def load_model(model_name):
+        classifier.choose_model(model_name)
+    model_list = ["resnet34", "mobile_net_v3_large"]
+    model_name = st.sidebar.selectbox(
+        "Model name", model_list)
+    load_model(model_name)
 
-            # Display result
-            col1, col2, col3 = st.columns(3, gap="small")
-            col1.text(f"face {index + 1}:")
-            col2.image(face["image"],
-                       width=face["image"].width, channels="RGB")
-            col3.image(remove_makeup, width=remove_makeup.width,
-                       channels="RGB")
+    # Process input image
+    if image_list is not None:
+        for image in image_list:
+            st.markdown("--------------------------------------------------------")
+            our_image = Image.open(image).convert("RGB")
+            st.markdown("Your input image:")
+            st.image(our_image, channels="RGB")
+            faces = face_detector.detect_face(our_image)
+            information = f"**Detect {len(faces)} faces:**"
+            st.markdown(information)
 
+            for index, face in enumerate(faces):
+                # Remove makeup
+                predict_class, proba = classifier.predict(face["image"])
+                if predict_class[0] == 1:
+                    info = "makeup detected"
+                    remove_makeup = makeup_remover.remove_makeup(face["image"])
+                    remove_makeup = Image.fromarray(
+                        (remove_makeup*255).astype(np.uint8)).resize((face["width"], face["height"]))
+                else:
+                    info = "No makeup detected."
+                    remove_makeup = face["image"]
+
+                # Display result
+                col1, col2, col3 = st.columns(3, gap="medium")
+                col1.text(f"face {index + 1}:")
+                col2.image(face["image"], caption="Original",
+                        width=face["image"].width, use_column_width="auto", channels="RGB")
+                col3.image(remove_makeup, caption=info, width=remove_makeup.width, use_column_width="auto",
+                        channels="RGB")
 
 if __name__ == "__main__":
     main()
